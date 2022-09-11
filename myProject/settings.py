8cccc66bf80d87dc319b709f07bbf435c5a8fc0f
Sumlycode <charin55220012@gmail.com>
2022-09-11 18:55:36 +0700

    'uploadlocallhosts'

--- myProject/settings.py
+++ myProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5g-$)pxvoq-((1eti1*%+=s%s#g6ptls5dfrdm*tuse(dw*nou
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['temm.herokuapp.com','127.0.0.1']
 
 
 # Application definition
